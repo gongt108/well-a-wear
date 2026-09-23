75242c24765bc7b5ec7cb35ad43d664429732dcd
code <code@Tiffanys-MacBook-Pro.local>
2026-09-22 23:14:32 -0700

    style mini calendar

--- src/app/calendar.tsx
+++ src/app/calendar.tsx
@@ -1,14 +1,28 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView, StyleSheet, Text } from "react-native";
 import MiniCalendar from "../components/MiniCalendar";
 import { globalStyles } from "../styles/global";
+
+const markedDates = ['2026-08-01', '2026-08-05', '2026-09-10'];
 
 export default function CalendarScreen() {
   return (
     <ScrollView style={globalStyles.container}>
         <Text style={globalStyles.title}>Calendar</Text>
         <Text style={globalStyles.empty}>No upcoming events.</Text>
-        <MiniCalendar />
+        <MiniCalendar markedDates={markedDates} />
 
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  markedDateColor: {
+    backgroundColor: '#4fc3f7',
+
+  }
+});
--- src/components/MiniCalendar.tsx
+++ src/components/MiniCalendar.tsx
@@ -1,29 +1,57 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
-export default function MiniCalendar() {
+export default function MiniCalendar({ markedDates }) {
+
+  const [datesWithOutfits, setDatesWithOutfits] = useState();
+
+  useEffect(() => {
+    const retrievedDatesWithOutfits = markedDates.reduce((acc, date) => {
+      acc[date] = { 
+        marked: true, 
+        color: '#4fc3f7', 
+        textColor: 'white', 
+        startingDay: true, 
+        endingDay: true
+      };
+      return acc;
+    }, {});
+
+    setDatesWithOutfits(retrievedDatesWithOutfits);
+  }, [markedDates]);
+
   return (
-    <View>
-      <Text>Mini Calendar</Text>
+    <View style={styles.container}>
       <Calendar
+        markingType={'period'}
         onDayPress={day => {
           console.log('selected day', day);
         }}
         style={{
-    borderWidth: 1,
-    borderColor: 'gray',
-    height: 350,
-    borderRadius: 10,
-  }}
+          borderWidth: 1,
+          borderColor: 'gray',
+          height: 350,
+          width: 350,
+          borderRadius: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+      
+        }}
+        markedDates={datesWithOutfits}
       />
     </View>
   );
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-}
+  markedDate: {
+    backgroundColor: '#4fc3f7',
+    color: 'white',
+  }
+});
